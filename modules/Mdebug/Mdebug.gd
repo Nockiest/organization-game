f16cba8d8e6f772fe0466f3042fc9e9799794e63
Andrew <ondralukes06@seznam.cz>
2024-02-25 16:30:10 +0100

    added movement

--- modules/Mdebug/Mdebug.gd
+++ modules/Mdebug/Mdebug.gd
@@ -24,15 +24,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	# Update the label text with debug values
 	for child in self.get_child(0).get_children():
-		child.text =  get_viewport().get_mouse_position() 
+		child.text =  str(get_viewport().get_mouse_position()) 
 
-# Function to update debug values with mouse position
-func displayValues() -> void:
-	# Get the mouse position relative to the viewport
-	var mouse_position = get_viewport().get_mouse_position()
-	# Set the mouse position to debug_value1
-	debug_value1 = int(mouse_position.x)
-	debug_value2 = float(mouse_position.y)
-	debug_value3 = str(mouse_position)
-
-	
+ 
