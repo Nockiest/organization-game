7e75e59ea2f6a10c0fdae116e65f14d53a060730
Andrew <ondralukes06@seznam.cz>
2024-03-01 16:08:12 +0100

    image for tutorial

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -4,15 +4,10 @@ extends CharacterBody2D
 	set(value):
 		$MkeyMovement.boundaries= value
 		allowed_movement = value
-
-# Reference to the CollisionShape2D node
+ 
 var collision_shape: CollisionShape2D
 
 func _draw() -> void:
-	# Set the Z-index for the drawing
- 
-
-	# Draw the stuff here
 	draw_circle(Vector2.ZERO, 5 , Color("black"))
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,26 +19,13 @@ func _ready() -> void:
 func _on_body_entered(body: Node2D) -> void:
 	print('body collided with player')
 	$thud.play()
-	# Adjust the draw shape to match the collision shape
-#	update_draw_shape()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
-
-
-
-#
 func _on_area_entered(area: Area2D) -> void:
 	print('player collided')
-
 
 func _on_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
 	print('player collided with body')
 
-
- 
-#
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	print('body collided with player')
 	$thud.play()
